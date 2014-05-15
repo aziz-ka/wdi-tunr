582180ab9c63aae7c926d4e8ac001bda1080ccf0
aziz-ka <hasanov.aziz@gmail.com>
2014-05-14 23:24:15 -0400

    fully styled tunr app

--- Gemfile
+++ Gemfile
@@ -47,4 +47,5 @@ end
 gem 'rails_12factor'
 
 gem 'httparty'
+gem 'font-awesome-rails'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  # wont allow more than 1 listener at a time
+  def current_listener
+    @current_listener = Listener.first
+  end
 end
